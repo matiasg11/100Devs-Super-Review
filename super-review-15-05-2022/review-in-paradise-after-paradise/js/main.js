22983add1b729a069fee2cab26b0e9c44de7de45
MatiasG11 <matiasguarnera@gmail.com>
2022-05-15 20:45:34 -0300

    Arrays, check!

--- super-review-15-05-2022/review-in-paradise-after-paradise/js/main.js
+++ super-review-15-05-2022/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,23 @@
-// Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+/*Create a function that takes in an array. 
+If the first number, is less than the last number, alert "Hi". 
+If the first number is greater than the last number, alert "Bye". 
+If they are equal, alert "We close in an hour".*/
+
+function paradise(array){
+    if (array[0]<array[array.length-1]){
+        alert("Hi")
+    }else if (array[0]>array[array.length-1]){
+        alert("Bye")
+    }else {
+        alert("We close in an hour")
+    }
+}
+paradise ([1,0]) //bye
+
+paradise ([0,1]) //hi
+
+paradise ([1,1]) //we close
+
+paradise ([1,0,1]) // we close
+paradise ([1,0,0]) // bye
+paradise ([0,0,1]) // hi
--- super-review-15-05-2022/review-winter-games/js/main.js
+++ super-review-15-05-2022/review-winter-games/js/main.js
@@ -1,1 +1,15 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function arrayEvens(arr){
+    let evens = []
+    arr.forEach(element => { 
+        if(element%2==0){
+            evens.push(element)
+        }
+    });
+    console.log(evens)
+    return evens
+    
+}
+
+arrayEvens([0,2,1,4,2,5,61,2,3,6,33])
