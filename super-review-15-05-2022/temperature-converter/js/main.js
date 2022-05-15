c9cdfb3d95152f57ec8713fc688be6e28d812dd1
MatiasG11 <matiasguarnera@gmail.com>
2022-05-15 18:49:56 -0300

    Bachelor 1 and 2 - Done! Still have to review temperature converter

--- super-review-15-05-2022/temperature-converter/js/main.js
+++ super-review-15-05-2022/temperature-converter/js/main.js
@@ -1,1 +1,17 @@
-//Write your pseduo code first! 
+//Write your pseudo code first! 
+
+//Need an input value.
+//It should convert from C to F and not backwards.
+//The formula is C*(9/5)+32
+//It must return a number
+
+function celsiusToFahrenheit(celsius){
+    
+    //The formula is C*(9/5)+32
+
+    //It must return a number
+}
+
+celsiusToFahrenheit(-40) //-40
+celsiusToFahrenheit(0) //32
+celsiusToFahrenheit(100) //212
