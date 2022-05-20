4228600697a85e36a48f6d3c073a367751d67d40
MatiasG11 <matiasguarnera@gmail.com>
2022-05-20 15:13:25 -0300

    Small error corrected

--- super-review-15-05-2022/objects-listen-to-your-heart/js/main.js
+++ super-review-15-05-2022/objects-listen-to-your-heart/js/main.js
@@ -1,6 +1,6 @@
 //Create a stopwatch object that has four properties and three methods
 let stopwatch = {}
-    currentTime:11,
+    
     stopwatch.shape = "round"
     stopwatch.color = "black"
     stopwatch.brand = "Nike"
