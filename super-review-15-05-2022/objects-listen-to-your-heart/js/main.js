630e15c2da012b13dc2975806b67981951015c34
MatiasG11 <matiasguarnera@gmail.com>
2022-05-20 15:13:07 -0300

    Stopwatch object created

--- super-review-15-05-2022/objects-listen-to-your-heart/js/main.js
+++ super-review-15-05-2022/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,17 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {}
+    currentTime:11,
+    stopwatch.shape = "round"
+    stopwatch.color = "black"
+    stopwatch.brand = "Nike"
+    stopwatch.light = true
+
+    stopwatch.start = function(){
+        console.log("Run!!!")
+   }
+   stopwatch.stop = function(){
+       console.log("Rest!")
+   }
+   stopwatch.telltime = function (){
+       console.log("It's RUN O'Clock!")
+   }
