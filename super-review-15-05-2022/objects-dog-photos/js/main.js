c843b421623223ccbaa85d12ddd6cf0c0b75e496
MatiasG11 <matiasguarnera@gmail.com>
2022-05-22 22:02:35 -0300

    Finally, all done! Congrats to me!

--- super-review-15-05-2022/objects-dog-photos/js/main.js
+++ super-review-15-05-2022/objects-dog-photos/js/main.js
@@ -1,1 +1,11 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+const url = "https://dog.ceo/api/breeds/image/random"
+fetch(url)
+    .then(res => res.json()) //parse response as a JSON
+    .then(data =>{
+        console.log(data.message)
+        document.querySelector('img').src = data.message
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
